5873ecf1adf9691e9f7cf0df75fa127ff75ec33e
Evan Lin <evan.lin@binance.com>
2021-02-14 18:02:12 +0800

    feat: 新建角色和权限表

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,33 +5,39 @@ import { UserModule } from '@modules/user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getMetadataArgsStorage } from 'typeorm';
+import { PermissionModule } from '@modules/permission/permission.module';
+import { RoleModule } from '@modules/role/role.module';
 
 @Module({
   imports: [
     UserModule,
+    RoleModule,
+    PermissionModule,
     ConfigModule.forRoot({
-      envFilePath: '.env.local',
-      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigService],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: process.env.TYPEORM_HOST,
-        port: configService.get<number>('TYPEORM_PORT'),
-        username: process.env.TYPEORM_USERNAME,
-        password: process.env.TYPEORM_PASSWORD,
-        database: process.env.TYPEORM_DATABASE,
-        // timezone: '+08:00',
-        charset: 'utf8mb4',
-        entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
-        synchronize: true,
-        logging: true,
-      }),
-    }),
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: process.env.TYPEORM_HOST,
+          port: configService.get<number>('TYPEORM_PORT'),
+          username: process.env.TYPEORM_USERNAME,
+          password: process.env.TYPEORM_PASSWORD,
+          database: process.env.TYPEORM_DATABASE,
+          // timezone: '+08:00',
+          charset: 'utf8mb4',
+          entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
+          synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+          logging: process.env.TYPEORM_LOGGING === 'true'
+        };
+      }
+    })
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
